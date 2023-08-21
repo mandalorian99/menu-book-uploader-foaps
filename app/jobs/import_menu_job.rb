dde566336f25d7c6bd022a222e9da39de45f10da
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2023-08-21 10:51:24 +0530

    added parsing csv to import menu job

--- app/jobs/import_menu_job.rb
+++ app/jobs/import_menu_job.rb
@@ -1,8 +1,35 @@
+require 'csv'
+
 class ImportMenuJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(import)
     Rails.logger.debug "+++++++++++++++STARTING IMPORTER++++++++++++++"
+    blob = import.file
+
+    begin
+      blob.open do |file|
+        csv = CSV.read(file, headers: true)
+        row_count = csv.size
+        import.update(status: 'processing', total_rows: row_count)
+        csv.each_with_index do |row, index|
+          ## parse csv and insert into db 
+        end
+      end
+    rescue StandardError => e
+      meta = {"error": e.message, "backtrace": e.backtrace }
+      import.update(status: 'aborted', finished_at: DateTime.now, meta: meta)
+      return
+    end
+
+    import.update(status: 'finished', finished_at: DateTime.now)
+
+  end
+
+
+  private
+
+  def calc_progress(progress, total_rows)
+    ((progress.to_f/total_rows) * 100).to_i
   end
 end
